45c77ea9d0ee763acd5e681d3c96701e06004e4b
raditya putranto <radityaputranto97@yahoo.co.id>
2018-05-15 20:26:19 +0700

    fix layout user

--- kehutanan/resources/views/user/profil.blade.php
+++ kehutanan/resources/views/user/profil.blade.php
@@ -32,7 +32,8 @@
                     <div class="col-lg-4 col-xlg-3 col-md-5">
                         <div class="card">
                             <div class="card-block">
-                                <center class="m-t-30"> <img src="assets/profil.jpg" class="img-circle" width="150" />
+                                <center class="m-t-30"> 
+                                    <!-- <img src="assets/profil.jpg" class="img-circle" width="150" /> -->
                                     <h4 class="card-title m-t-10">Raditya Putranto</h4>
                                     <H6>NIP : 2103151060 </H6>
                                     
@@ -48,9 +49,11 @@
                                 <h6>radityaputranto97@gmail.com</h6> <small class="text-muted p-t-30 db">Phone</small>
                                 <h6>+62 877 03221 344</h6> <small class="text-muted p-t-30 db">Address</small>
                                 <h6>GMS EA V / 28 , Waru Sidoarjo 61256</h6>
-                                <div class="map-box">
+                                <br>
+                                <small class="text-muted p-t-30 db">Terakhir diperbaruhi : 02-05-2018</small>
+                                <!-- <div class="map-box">
                                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.915395246305!2d112.7740572153329!3d-7.363380374502246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7e53d20055241%3A0x1a9a79629be45c8!2sInfinity+Teknik+Solusindo!5e0!3m2!1sen!2sin!4v1517036380046" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </div>
@@ -61,85 +64,60 @@
                             <!-- Nav tabs -->
                             <ul class="nav nav-tabs profile-tab" role="tablist">
                                 
-                                <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#profile" role="tab">Profile</a> </li>
-                                <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#update" role="tab">Update</a> </li>
+                                
+                                <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#update" role="tab">Update</a> </li>
+                                <!-- <li class="nav-item"> <a class="nav-link " data-toggle="tab" href="#profile" role="tab">Profile</a> </li> -->
                             </ul>
                             <!-- Tab panes -->
-                            <div class="tab-content">
+                            <!-- <div class="tab-content"> -->
                                 
                                 <!--firest tab-->
-                                <div class="tab-pane active" id="profile" role="tabpanel">
-                                    <div class="card-block">
-                                        <div class="row">
-                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Nama Lengkap</strong>
-                                                <br>
-                                                <p class="text-muted">Raditya Putranto</p>
-                                            </div>
-                                            <div class="col-md-3 col-xs-6 b-r"> <strong>No Telp</strong>
-                                                <br>
-                                                <p class="text-muted">+62 877 03221 344</p>
-                                            </div>
-                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Email</strong>
-                                                <br>
-                                                <p class="text-muted">raditya@gmail.com</p>
-                                            </div>
-                                            <div class="col-md-3 col-xs-6"> <strong>Lokasi</strong>
-                                                <br>
-                                                <p class="text-muted">Surabaya</p>
-                                            </div>
-                                        </div>
-                                        <hr>
-                                        <p class="m-t-30">Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.</p>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries </p>
-                                        <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                                       
-                                    </div>
-                                </div>
+                              
 
-                                <div class="tab-pane" id="update" role="tabpanel">
+                               <!--  <div class="tab-pane" id="update" role="tabpanel"> -->
                                     <div class="card-block">
                                         <form class="form-horizontal form-material">
                                             <div class="form-group">
                                                 <label class="col-md-12">Nama Lengkap</label>
                                                 <div class="col-md-12">
-                                                    <input type="text" placeholder="Raditya Putranto" class="form-control form-control-line">
+                                                    <input type="text" placeholder="Raditya Putranto" class="form-control form-control-line" name="nama">
                                                 </div>
                                             </div>
                                             
                                             <div class="form-group">
                                                 <label for="example-email" class="col-md-12">Email</label>
                                                 <div class="col-md-12">
-                                                    <input type="email" placeholder="raditya@gmail.com" class="form-control form-control-line" name="example-email" id="example-email">
+                                                    <input type="email" placeholder="raditya@gmail.com" class="form-control form-control-line" name="email" id="example-email">
                                                 </div>
                                             </div>
                                             <div class="form-group">
                                                 <label class="col-md-12">Password</label>
                                                 <div class="col-md-12">
-                                                    <input type="password" value="password" class="form-control form-control-line">
+                                                    <input type="password" value="password" class="form-control form-control-line" name="password">
                                                 </div>
                                             </div>
                                             <div class="form-group">
                                                 <label class="col-md-12">No Telephone</label>
                                                 <div class="col-md-12">
-                                                    <input type="text" placeholder="123 456 7890" class="form-control form-control-line">
+                                                    <input type="number" placeholder="123 456 7890" class="form-control form-control-line" name="notelp">
                                                 </div>
                                             </div>
                                             <div class="form-group">
                                                 <label class="col-md-12">Alamat</label>
                                                 <div class="col-md-12">
-                                                    <textarea rows="5" class="form-control form-control-line"></textarea>
+                                                    <textarea rows="5" class="form-control form-control-line" name="alamat"></textarea>
                                                 </div>
                                             </div>
                                           
                                             <div class="form-group">
                                                 <div class="col-sm-12">
-                                                    <button class="btn btn-success">Update Profil</button>
+                                                    <button class="btn btn-success" type="submit">Update Profil</button>
                                                 </div>
                                             </div>
                                         </form>
                                     </div>
-                                </div>
-                            </div>
+                               <!--  </div> -->
+                            <!-- </div> -->
                         </div>
                     </div>
                     <!-- Column -->
